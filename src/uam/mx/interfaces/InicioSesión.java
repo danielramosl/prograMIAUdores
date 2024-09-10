027df53b425e8069c7385aa02dc1e4b511ad485d
danielramosl <91300869+danielramosl@users.noreply.github.com>
2024-09-10 08:04:20 -0600

    Update InicioSesión.java

--- src/uam/mx/interfaces/InicioSesión.java
+++ src/uam/mx/interfaces/InicioSesión.java
@@ -9,7 +9,7 @@ package uam.mx.interfaces;
  * @author HP
  */
 public class InicioSesión extends javax.swing.JFrame {
-    String s = "Hola mundo";
+    String s = "Hola gatitos";
     /**
      * Creates new form InicioSesión
      */
