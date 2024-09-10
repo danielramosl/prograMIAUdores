3b82a7b089cd05cd3a141d4236c0d1d56621b6dc
PrograMIAUdores <HP@DESKTOP-L2NUAVR>
2024-09-10 08:03:00 -0600

    hola mundo

--- src/uam/mx/interfaces/InicioSesión.java
+++ src/uam/mx/interfaces/InicioSesión.java
@@ -9,7 +9,7 @@ package uam.mx.interfaces;
  * @author HP
  */
 public class InicioSesión extends javax.swing.JFrame {
-
+    String s = "Hola mundo";
     /**
      * Creates new form InicioSesión
      */
